a339e00f9e49852bb8f24fb966a960fe02e8371d
Dante Tobar <dante.m.tobar@gmail.com>
2020-03-21 11:35:10 -0700

    create react express

---
+++ server/index.js
@@ -0,0 +1,29 @@
+// Basic Routes
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// Default port is 3000
+const PORT = process.env.PORT || 3000;
+
+// Logging Middleware
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+// JSON request body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Static Middleware
+app.use(express.static(path.join(__dirname, '../public')))
+
+// Route catchall
+app.get('*', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Set server to listen on PORT
+app.listen(PORT, () => {
+    console.log('Listening on port', PORT)
+})
