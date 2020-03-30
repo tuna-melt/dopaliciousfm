d0b53bdbadb216c1c4f226c0e4519163950e4352
Dante Tobar <dante.m.tobar@gmail.com>
2020-03-30 10:10:59 -0700

    seeds db with three users each with a comment

---
+++ scripts/seed.js
@@ -0,0 +1,41 @@
+const { db } = require('../server/db');
+
+const User = db.model('User');
+const Comment = db.model('Comment');
+const Reaction = db.model('Reaction');
+
+// Find and drop all collection
+User.collection.drop();
+Comment.collection.drop();
+// Reaction.collection.drop();
+
+const runSeed = async () => {
+  const users = await User.create([
+    {
+      name: 'Dante',
+    },
+    {
+      name: 'Diego',
+    },
+    {
+      name: 'Luna',
+    },
+  ]);
+
+  const comments = await Comment.create([
+    {
+      content: 'hi',
+      user: users[0]._id,
+    },
+    {
+      content: 'hello',
+      user: users[1]._id,
+    },
+    {
+      content: 'oh hey',
+      user: users[2]._id,
+    },
+  ]);
+};
+
+runSeed().then(() => db.close());
