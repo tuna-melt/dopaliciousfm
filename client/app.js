7af0bec0a42fd5f76f2bd1b50e93c75b44e140a5
Dante Tobar <dante.m.tobar@gmail.com>
2020-03-30 11:19:58 -0700

    name change

---
+++ client/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/Root';
+import { Provider } from 'react-redux';
+import store from './store';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Root />
+  </Provider>,
+  document.getElementById('app')
+);
