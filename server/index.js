52604dcccd00dd7f95ab65be34a272813e71df55
Dante Tobar <dante.m.tobar@gmail.com>
2020-03-30 10:57:28 -0700

    socket added to server

--- server/index.js
+++ server/index.js
@@ -4,10 +4,23 @@ const path = require('path');
 const express = require('express');
 const { db } = require('./db');
 
-const app = express();
-
 // Default port is 3000
 const PORT = process.env.PORT || 3000;
+
+// Set server to listen on PORT
+const app = express();
+const server = app.listen(PORT, () => {
+  console.log(
+    `
+
+    Listening on port ${PORT}
+  `
+  );
+});
+
+// Set socket connection
+const io = require('socket.io')(server);
+require('./socket')(io);
 
 // Logging Middleware
 const morgan = require('morgan');
@@ -26,14 +39,4 @@ app.use('/api', require('./api'));
 // Route catchall
 app.get('*', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
-// Set server to listen on PORT
-app.listen(PORT, () => {
-  console.log(
-    `
-
-    Listening on port ${PORT}
-  `
-  );
 });
