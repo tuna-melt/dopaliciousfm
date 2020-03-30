a4973e44ba83ac14374d8e3abc95a91e8c58a0c3
Dante Tobar <dante.m.tobar@gmail.com>
2020-03-30 11:20:40 -0700

    added redux to deal with comments

--- client/socket.js
+++ client/socket.js
@@ -1,11 +1,14 @@
 import io from 'socket.io-client';
+import { addComment } from './store';
 
 const socket = io(window.location.origin);
 
 socket.on('connect', () => {
   console.log('socket is connected');
 
-  socket.on('new-message', data => {});
+  socket.on('new-message', data => {
+    addComment(data);
+  });
 });
 
 export default socket;
---
+++ client/store.js
@@ -0,0 +1,69 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import axios from 'axios';
+import socket from './socket';
+
+// Actions
+const GOT_COMMENTS_FROM_SERVER = 'GOT_COMMENTS_FROM_SERVER';
+const gotComments = comments => {
+  return { type: GOT_COMMENTS_FROM_SERVER, comments };
+};
+
+const ADD_COMMENT = 'ADD_COMMENT';
+export const addComment = comment => {
+  return {
+    type: ADD_COMMENT,
+    comment,
+  };
+};
+
+// Thunks
+export const getComments = () => {
+  return async dispatch => {
+    try {
+      const response = await axios.get('/api/comments');
+      const messages = response.data;
+      dispatch(gotComments(messages));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const postComment = (comment, user) => {
+  return async dispatch => {
+    try {
+      const response = await axios.post('/api/comment', {
+        content: comment,
+        user: user.id,
+      });
+      const newComment = response.data;
+      dispatch(addComment(newComment));
+      socket.emit('new-message', newComment);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+// Reducer
+const initialState = { messages: [] };
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GOT_COMMENTS_FROM_SERVER:
+      return {
+        ...state,
+        messages: action.messages,
+      };
+    case ADD_COMMENT:
+      return {
+        ...state,
+        messages: [...state.messages, action.message],
+      };
+    default:
+      return state;
+  }
+};
+
+export default createStore(reducer, applyMiddleware(thunkMiddleware));
