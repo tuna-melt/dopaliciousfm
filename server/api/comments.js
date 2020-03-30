985835856efe05754a7044b5f1228f26939b4eb1
Dante Tobar <dante.m.tobar@gmail.com>
2020-03-30 10:56:45 -0700

    comment post route

--- server/api/comments.js
+++ server/api/comments.js
@@ -2,8 +2,23 @@ const router = require('express').Router();
 const Comment = require('mongoose').model('Comment');
 
 router.get('/', async (req, res, next) => {
-  const comments = await Comment.find({}).populate('user');
-  res.send(comments);
+  try {
+    const comments = await Comment.find({}).populate('user');
+    res.send(comments);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const { newComment } = req.body;
+    const comment = await Comment.create({ newComment });
+    console.log(comment);
+    res.send(comment);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
