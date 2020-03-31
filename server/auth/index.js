7eb8907e9473cc9c7dede4728c5b982ca95c7661
Dante Tobar <dante.m.tobar@gmail.com>
2020-03-30 17:16:42 -0700

    added me route that gives user

--- server/auth/index.js
+++ server/auth/index.js
@@ -2,4 +2,16 @@ const router = require('express').Router();
 
 router.use('/spotify', require('./spotify'));
 
+router.get('/me', (req, res, next) => {
+  try {
+    if (req.user._id) {
+      res.send(req.user);
+    } else {
+      res.send({});
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
