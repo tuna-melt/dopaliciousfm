333ab820d146813e68c41180dec6c5b3b2c7e77e
Dante Tobar <dante.m.tobar@gmail.com>
2020-04-06 13:58:18 -0700

    gif added

---
+++ client/components/Visualizer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Visualizer = props => {
+  return (
+    <img
+      src="https://media.giphy.com/media/HOX80nO0h8Na/source.gif"
+      className="visualizer"
+    />
+  );
+};
+
+export default Visualizer;
