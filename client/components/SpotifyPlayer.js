4110cb4a60b8ba4546a7941c7b9aba73a5336659
Dante Tobar <dante.m.tobar@gmail.com>
2020-05-09 10:14:57 -0700

    song vis as rectangle

--- client/components/SpotifyPlayer.js
+++ client/components/SpotifyPlayer.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { connect } from 'react-redux';
 
 const SpotifyPlayer = props => {
   const { currentSong } = props;
 
-  const sizeSVG = () => {};
+  const mask = useRef(null);
+  const rect = useRef(null);
+  const text = useRef(null);
+
+  const sizeSVG = () => {
+    if (text.current && rect.current) {
+      const textBox = text.current.getBBox();
+
+      const padding = 30;
+      let width = textBox.width + padding;
+      const height = textBox.height + padding;
+
+      if (width < 500) width = 500;
+
+      rect.current.setAttribute('width', width);
+      rect.current.setAttribute('height', height);
+      mask.current.setAttribute('width', width);
+      mask.current.setAttribute('height', height);
+      text.current.setAttribute('x', width / 2);
+      text.current.setAttribute('y', height / 2);
+    }
+  };
+
+  useEffect(sizeSVG);
 
   return (
     <div id="player">
@@ -13,14 +36,20 @@ const SpotifyPlayer = props => {
           <svg id="song-title">
             <defs>
               <mask id="mask">
-                <rect width="100%" fill="#fff" />
-                <text fill="#000" fontSize="50" transform="translate(0,50)">
+                <rect fill="#fff" ref={mask} />
+                <text
+                  fill="#000"
+                  fontSize="50"
+                  textAnchor="middle"
+                  dominantBaseline="middle"
+                  ref={text}
+                >
                   {currentSong.name}
                 </text>
               </mask>
             </defs>
 
-            <rect width="100%" fill="#fff" />
+            <rect width="100%" fill="#fff" ref={rect} />
           </svg>
         </React.Fragment>
       )}
