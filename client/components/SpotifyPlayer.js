57fa792076d2eceebc9f19d7e30413a3f89a6b71
Dante Tobar <dante.m.tobar@gmail.com>
2020-05-09 09:23:56 -0700

    player redesign

--- client/components/SpotifyPlayer.js
+++ client/components/SpotifyPlayer.js
@@ -1,71 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { setPlayer } from '../store';
-
 const SpotifyPlayer = props => {
-  const mountMusicPlayer = token => {
-    if (window.Spotify) {
-      const player = new window.Spotify.Player({
-        name: 'Dopaliscious Radio',
-        getOAuthToken: cb => {
-          cb(token);
-        },
-      });
+  const { currentSong } = props;
 
-      // Error handling
-      player.addListener('initialization_error', ({ message }) => {
-        console.error(message);
-      });
-      player.addListener('authentication_error', ({ message }) => {
-        console.error(message);
-      });
-      player.addListener('account_error', ({ message }) => {
-        console.error(message);
-      });
-      player.addListener('playback_error', ({ message }) => {
-        console.error(message);
-      });
+  const sizeSVG = () => {};
 
-      // Playback status updates
-      player.addListener('player_state_changed', state => {});
-
-      // Ready
-      player.addListener('ready', ({ device_id }) => {
-        console.log('player is ready');
-        props.setDevice(device_id);
-      });
-
-      // Not Ready
-      player.addListener('not_ready', ({ device_id }) => {
-        console.log('Device ID has gone offline', device_id);
-      });
-
-      // Connect to the player!
-      player.connect();
-    }
-  };
-
-  const { user, deviceId, currentSong } = props;
-  if (user && user.accessToken && !deviceId) {
-    mountMusicPlayer(props.user.accessToken);
-  }
-
-  let albumImg = '';
-  if (currentSong.name) {
-    albumImg = currentSong.album.images[1];
-  }
   return (
     <div id="player">
-      {props.currentSong.name && (
+      {currentSong.name && (
         <React.Fragment>
-          <img className="cover" src={albumImg.url} />
-          <div className="info">
-            <h3>{props.currentSong.name}</h3>
-            <h3>
-              {props.currentSong.artists.map(artist => artist.name).join(' ')}
-            </h3>
-          </div>
+          <svg id="song-title">
+            <defs>
+              <mask id="mask">
+                <rect width="100%" fill="#fff" />
+                <text fill="#000" fontSize="50" transform="translate(0,50)">
+                  {currentSong.name}
+                </text>
+              </mask>
+            </defs>
+
+            <rect width="100%" fill="#fff" />
+          </svg>
         </React.Fragment>
       )}
     </div>
@@ -74,16 +30,8 @@ const SpotifyPlayer = props => {
 
 const mapState = state => {
   return {
-    user: state.user,
     currentSong: state.currentSong,
-    deviceId: state.deviceId,
   };
 };
 
-const mapDispatch = dispatch => {
-  return {
-    setDevice: deviceId => dispatch(setPlayer(deviceId)),
-  };
-};
-
-export default connect(mapState, mapDispatch)(SpotifyPlayer);
+export default connect(mapState, null)(SpotifyPlayer);
