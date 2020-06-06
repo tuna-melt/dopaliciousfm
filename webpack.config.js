3a66bf30dd8466e128f524a2846f188ebaed1669
Dante Tobar <dante.m.tobar@gmail.com>
2020-06-06 13:23:11 -0700

    sass dependencies added

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
   },
 };
