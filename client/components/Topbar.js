34e09dd456633ab3b3bda8abd5791fcf0592c950
Dante Tobar <dante.m.tobar@gmail.com>
2020-06-06 11:56:41 -0700

    logout button added

--- client/components/Topbar.js
+++ client/components/Topbar.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Topbar = () => {
+import { logout } from '../store';
+
+const Topbar = props => {
+  const logoutUser = () => {
+    if (props.spotifyPlayer._options) props.spotifyPlayer.disconnect();
+    props.logout();
+  };
   return (
     <div id="topbar">
       <h1>DOPALISCIOUS</h1>
+      <a onClick={() => logoutUser()}>Logout</a>
     </div>
   );
 };
 
-export default Topbar;
+const mapDispatch = dispatch => ({
+  logout: () => dispatch(logout()),
+});
+
+export default connect(null, mapDispatch)(Topbar);
