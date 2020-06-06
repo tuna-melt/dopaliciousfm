ffe312c7cf539c24b0494759f7aa976bcc8026dc
Dante Tobar <dante.m.tobar@gmail.com>
2020-06-06 11:59:22 -0700

    login/logout thunks added to store

--- client/store/user.js
+++ client/store/user.js
@@ -1,27 +1,53 @@
 import axios from 'axios';
 
-const LOGIN = 'LOGIN';
-export const login = user => {
-  return { type: LOGIN, user };
+const SET_USER = 'SET_USER';
+const setUser = user => {
+  return { type: SET_USER, user };
 };
 
-const LOGOUT = 'LOGOUT';
-export const logout = () => {
-  return { type: LOGOUT };
+export const logout = () => async dispatch => {
+  try {
+    await axios.post('/auth/logout');
+    dispatch(setUser({}));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const login = user => async dispatch => {
+  try {
+    const { data } = await axios.post('/auth/login', user);
+    if (data && data._id) {
+      dispatch(setUser(data));
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const signup = user => async dispatch => {
+  try {
+    const { data } = await axios.post('/auth/signup', user);
+    if (data && data._id) {
+      dispatch(setUser(data));
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const me = () => async dispatch => {
   try {
     const { data } = await axios.get('/auth/me');
     if (data) {
-      if (data._id) {
+      if (data.tokenExpiration) {
         setTimeout(() => {
           dispatch(me());
         }, data.tokenExpiration - Date.now() + 5000);
       }
-      dispatch(login(data));
+      dispatch(setUser(data));
     } else {
-      dispatch(login({}));
+      dispatch(setUser({}));
     }
   } catch (err) {
     console.log(err);
@@ -31,11 +57,8 @@ export const me = () => async dispatch => {
 // reducer
 export default (user = {}, action) => {
   switch (action.type) {
-    case LOGIN:
+    case SET_USER:
       return action.user;
-
-    case LOGOUT:
-      return {};
 
     default:
       return user;
