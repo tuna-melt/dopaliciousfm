1101509c4e54b5055ed1636926c62c2530cde350
Dante Tobar <dante.m.tobar@gmail.com>
2020-06-06 11:56:59 -0700

    auth forms added to routes

--- client/routes.js
+++ client/routes.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 
 import Login from './components/Login';
 import Play from './components/Play';
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
 
 const Routes = props => {
   const { isLoggedIn } = props;
@@ -11,6 +13,8 @@ const Routes = props => {
   return (
     <Switch>
       {isLoggedIn && <Route path="/" component={Play} />}
+      {!isLoggedIn && <Route path="/login" component={LoginForm} />}
+      {!isLoggedIn && <Route path="/signup" component={SignupForm} />}
       {!isLoggedIn && <Route path="/" component={Login} />}
     </Switch>
   );
