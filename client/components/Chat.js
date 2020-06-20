07ec8d6bcda61d60210ec5f7e85948bcaa1200a2
Dante Tobar <dante.m.tobar@gmail.com>
2020-06-20 11:47:25 -0700

    chat expands from comment bubble

--- client/components/Chat.js
+++ client/components/Chat.js
@@ -3,6 +3,9 @@ import '../styles/chat.scss';
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCommentAlt, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+
 import Feed from './Feed';
 import TextBar from './TextBar';
 
@@ -12,15 +15,36 @@ const Chat = props => {
   const [isVisible, setVisible] = useState(false);
 
   return (
-    <div id="chat" className={isVisible ? 'visible' : ''}>
+    <React.Fragment>
       <button
         type="button"
         id="toggleChat"
+        className={isVisible ? 'visible' : ''}
         onClick={() => setVisible(!isVisible)}
-      />
-      <Feed comments={comments} />
-      <TextBar />
-    </div>
+      >
+        <FontAwesomeIcon
+          icon={faCommentAlt}
+          className="fa-flip-horizontal"
+          size="2x"
+          color="#fff"
+        />
+      </button>
+      <div id="chat" className={isVisible ? 'visible' : ''}>
+        <div className="header">
+          <button
+            type="button"
+            className="colapse"
+            onClick={() => setVisible(!isVisible)}
+          >
+            <FontAwesomeIcon icon={faAngleDown} size="2x" />
+          </button>
+        </div>
+        <div className="content">
+          <Feed comments={comments} />
+          <TextBar />
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
